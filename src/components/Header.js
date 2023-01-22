aa9187d6f1e81ca3d0a7e3f01f0d7927e52a0403
Rohit Vishwakarma <53476944+Rohit77707@users.noreply.github.com>
2023-01-23 02:56:25 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+
+function Header() {
+    return (
+        <ul className="flex ml-auto w-full font-bold">
+            <li className="text-xs text-gray-800 ml-auto mr-6 border-b-2 border-green-400 cursor-pointer">Weather</li>
+            <li className="text-xs text-gray-800 mr-6 alert-notice cursor-pointer border-b-2 hover:border-green-400"></li>
+            
+        </ul>
+    )
+}
+
+export default Header
